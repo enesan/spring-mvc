9e9fe1d7de8f1971eb924338c6c0f55652091753
enesan <vlad.ru-2000@mail.ru>
2021-03-22 18:41:46 +0300

    Закончено

---
+++ spring-mvc/src/main/java/web/controller/CarController.java
@@ -0,0 +1,29 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    @Autowired
+    CarService carService;
+
+    @GetMapping(value = "/cars")
+    public String printWelcome(@RequestParam("count") int count, Model model) {
+        model.addAttribute("carsList", carService.getCars(count));
+
+        return "cars";
+    }
+
+}
